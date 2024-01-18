02cc01b2f363e0a3d2a5a95c7c433d235824f6b2
BenLalor <lalor.benjamin@gmail.com>
2024-01-18 12:19:36 -0700

    Update resume file

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -6,3 +6,5 @@ export const aboutData = {
     "During the day I work to develop and maintain Californias preferred Energy Code Complaince tool and also develop a database to provide insight on Californias building data. In the evening I develop web applications primarily usng React and Python. In my free time I can be found ourdoors with family or cheering on the SF Giants or 49ers. Right now I am focusing on developing my React and DJango skills.",
   image: 2,
 };
+
+// Trigger deploy
